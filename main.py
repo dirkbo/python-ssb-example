8ccdfe080cc05a52f8a6b321a86e1fb30f5522b2
Dirk B <dirkbo@googlemail.com>
2021-08-08 12:50:20 +0200

    Persistant Identity

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import json
 import sys
 
-
 from ssb.database import SsbDatabase
 from ssb.identity import SsbIdentity
 from ssb.message import SSbMessage
 
-# Older versions of Python don't respect dictionary insertion order.
-is_recent_python = sys.version_info >= (3, 6)
-assert is_recent_python, "Please upgrade to Python 3.6 or greater"
-
 
 def run_example():
+    # Older versions of Python don't respect dictionary insertion order.
+    is_recent_python = sys.version_info >= (3, 6)
+    assert is_recent_python, "Please upgrade to Python 3.6 or greater"
+
     ssb_id = SsbIdentity()
     ssb_db = SsbDatabase()
 
--- ssb/identity.py
+++ ssb/identity.py
@@ -1,6 +1,32 @@
+import json
 from nacl import signing as nacl_signing
 
 
 class SsbIdentity:
-    def __init__(self):
+    def __init__(self, id_file="identity.json"):
+        self.signing_key = ""
+        self.load_identity(id_file=id_file)
+
+    def create_new_identity(self, id_file="identity.json"):
         self.signing_key = nacl_signing.SigningKey.generate()
+        self.save_identity(id_file=id_file)
+
+    def load_identity(self, id_file="identity.json", create_if_not_exists: bool = True):
+        try:
+            with open(id_file) as jsonIdFile:
+                json_object = json.load(jsonIdFile)
+                seed = json_object["seed"].encode('latin-1')
+                self.signing_key = nacl_signing.SigningKey(seed=seed)
+        except FileNotFoundError:
+            if create_if_not_exists:
+                self.create_new_identity(id_file=id_file)
+
+    def save_identity(self, id_file="identity.json"):
+        with open(id_file, 'w') as jsonFile:
+            json.dump(self.dict, jsonFile)
+            jsonFile.close()
+
+    @property
+    def dict(self):
+        return {'seed': self.signing_key.__dict__['_seed'].decode('latin-1')}
+
